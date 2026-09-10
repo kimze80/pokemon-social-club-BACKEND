6eff12f387b5394e46ddfaccf9f38081b4013228
Jose Kim <kimze80@gmail.com>
2026-09-10 17:37:46 -0300

    adicionando cors file

---
+++ src/main/java/com/pokecards/api/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.pokecards.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Libera todas as rotas da API
+                .allowedOrigins("http://localhost:3000") // URL padrão do servidor do Next.js
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
